790959fee70e35633c6a5bfdc784c29088d2b395
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-07-24 21:32:24 +0530

    notifications added

--- src/views/wall/Wall.jsx
+++ src/views/wall/Wall.jsx
@@ -1,49 +1,50 @@
-// src/pages/Wall.js
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPosts } from "../../redux/postsSlice";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchPosts} from "../../redux/postsSlice";
+import {Bounce, toast} from "react-toastify"
 import Post from "../../components/Post";
 
 const Wall = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts.items);
-  const postStatus = useSelector((state) => state.posts.status);
-  const error = useSelector((state) => state.posts.error);
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts.items);
+    const postStatus = useSelector((state) => state.posts.status);
+    const error = useSelector((state) => state.posts.error);
 
-  useEffect(() => {
-    if (postStatus === "idle") {
-      dispatch(fetchPosts());
+    useEffect(() => {
+        if (postStatus === "idle") {
+            dispatch(fetchPosts());
+        }
+    }, [postStatus, dispatch]);
+
+    let content;
+
+    if (postStatus === "loading") {
+        content = <p>Loading...</p>;
+    } else if (postStatus === "succeeded") {
+
+        toast.success("Posts loaded successfully!", {
+            hideProgressBar: false, closeOnClick: true, pauseOnHover: true, theme: "dark", transition: Bounce,
+        });
+
+        content = (<ul>
+            {posts.map((post) => (<li key={post.id}>
+                <Post post={post}/>
+            </li>))}
+        </ul>);
+    } else if (postStatus === "failed") {
+
+        toast.error('Something went wrong!', {
+            hideProgressBar: false, closeOnClick: true, pauseOnHover: true, theme: "dark", transition: Bounce,
+        });
+        content = <p>{error}</p>;
     }
-  }, [postStatus, dispatch]);
 
-  let content;
-
-  if (postStatus === "loading") {
-    content = <p>Loading...</p>;
-  } else if (postStatus === "succeeded") {
-    content = (
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-    );
-  } else if (postStatus === "failed") {
-    content = <p>{error}</p>;
-  }
-
-  return (
-    <div
-      className={
-        "border border-black flex flex-col items-center justify-center"
-      }
+    return (<div
+        className={"border border-black flex flex-col items-center justify-center"}
     >
-      <h1 className={"text-4xl mt-3 font-bold"}>Wall</h1>
-      {content}
-    </div>
-  );
+        <h1 className={"text-4xl mt-3 font-bold"}>Wall</h1>
+        {content}
+    </div>);
 };
 
 export default Wall;
