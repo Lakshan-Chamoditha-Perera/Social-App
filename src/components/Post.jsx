46ac118d2b3688c2132e4a7f56d82fc2e6b5c4fa
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-07-24 20:52:59 +0530

    comment component updated

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,48 +6,44 @@ import {fetchComments} from "../redux/commentsSlice";
 const Post = ({post}) => {
     const dispatch = useDispatch();
     const [showComments, setShowComments] = useState(false);
-    const comments = useSelector((state) => state.comments.items);
+    const postComments = useSelector((state) => state.comments.items)?.filter(c => c.postId == post.id);
 
     const loadComments = async () => {
         setShowComments(true);
         dispatch(fetchComments());
-        // toast.success("Comments loaded successfully");
     };
 
     const hideComments = () => {
         setShowComments(false);
     };
 
-    return (<Card
-        className="cursor-pointer w-[350px] min-h-[400px] m-4 border border-gray-300 rounded-lg shadow-lg"
-        key={post.id}
-    >
+    return (<Card className=" w-[350px] min-h-[200px] m-4 border border-gray-300 rounded-lg shadow-lg" key={post.id}>
         {/* Card body */}
         <div className="p-4 flex flex-col justify-between">
             <h1 className="text-2xl font-bold">{post.title}</h1>
             <p>{post.content}</p>
             <div>
-                <div className={'w-full border d-flex items-end justify-end'}>
-                    <Button onClick={showComments ? hideComments: loadComments} variant="outlined"
-                            size={'small'}>
+                <div className="w-full  flex items-end justify-end">
+                    <Button onClick={showComments ? hideComments : loadComments} size={'small'}>
                         {showComments ? 'Hide comments' : 'Show comments'}
                     </Button>
                 </div>
+
             </div>
             {showComments ? (<div className="mt-4">
                 <h2 className="text-xl font-bold">Comments</h2>
                 <ul>
-                    {comments.map((comment) => {
-                        if (comment.postId == post.id) {
-                            return <li key={comment.id} className="mb-2">
-                                <p className="p-2 border border-gray-200 rounded">{comment.content}</p>
-                            </li>
-                        }
-                    })}
+                    {postComments.length > 0 ? (postComments.map((comment) => (
+                        <li key={comment.id} className="mb-2">
+                        <div className="p-2 border border-gray-200 rounded">
+                            <p>{comment.content}</p>
+                            <Button variant="outlined" size="small">Reply</Button>
+                        </div>
+                        </li>
+                    ))) : <li>No comments yet !</li>}
                 </ul>
-            </div>) : <>
-
-            </>}
+            </div>) : <></>
+            }
         </div>
     </Card>);
 };
